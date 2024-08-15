4057d6b9fd89a65eeab1936e86685a5df2c0f3d7
Gagan <gagangupta3130@gmail.com>
2024-08-15 21:41:07 +0530

    comment message

--- index.js
+++ index.js
@@ -30,6 +30,37 @@ app.get("/", (req, res) => {
   res.sendFile(__dirname + "/index.html");
 });
 
+// app.get("/video", async (req, res) => {
+//   try {
+//     const range = req.headers.range;
+//     if (!range) {
+//       res.status(400).json({ msg: "require range headers" });
+//     }
+//     const videoPath = path.join(__dirname, "public", "vimeo.mp4");
+//     const videoSize = fs.statSync(videoPath).size;
+//     const chunkSize = 10 ** 6; // 1 mb which we will be sending every time
+//     const startRange = Number(range.replace(/\D/g,"")) // replace all non digit character with empty string and return the number
+//     const endRange = Math.min(startRange + chunkSize, videoSize - 1) // we either take next chunk of 1 mb or leftover chunk whichever is minimum
+//     console.log(startRange, endRange, "values");
+//     const contentLength = endRange - startRange + 1;
+//     const headers = {
+//       "Content-Range": `bytes ${startRange}-${endRange}/${videoSize}`,
+//       "Accept-Ranges": "bytes",
+//       "Content-Length": contentLength,
+//       "Content-Type": "video/mp4",
+//     };
+//     res.writeHead(206, headers);
+//     const videoReadStream = fs.createReadStream(videoPath, {
+//       startRange,
+//       endRange,
+//     });
+//     videoReadStream.pipe(res);
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
+
+
 app.get("/video", function (req, res) {
     const range = req.headers.range;
     if (!range) {
@@ -38,8 +69,8 @@ app.get("/video", function (req, res) {
     const videoPath = "./public/vimeo.mp4";
     const videoSize = fs.statSync(videoPath).size;
     const CHUNK_SIZE = 10 ** 6;
-    const start = Number(range.replace(/\D/g, ""));
-    const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+    const start = Number(range.replace(/\D/g, "")); // replace all non digit character with empty string and return the number
+    const end = Math.min(start + CHUNK_SIZE, videoSize - 1); // we either take next chunk of 1 mb or leftover chunk whichever is minimum
     const contentLength = end - start + 1;
     const headers = {
         "Content-Range": `bytes ${start}-${end}/${videoSize}`,
